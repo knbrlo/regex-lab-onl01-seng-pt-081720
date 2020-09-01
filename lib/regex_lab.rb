edcc87bed6757690f1114e2a47524f330d2cd493
Ken Barlow <kenbrlow@gmail.com>
2020-09-01 05:10:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,7 +9,7 @@ end
 def words_five_letters_long(text)
   array_text = text.split(" ")
   puts "========================"
-  final_array = array_text.grep(/^\w{5}/)
+  final_array = array_text.grep(/^\w{5}$/)
   puts final_array
   final_array
   #puts text.scan(/\w{5}/)
