f9d48efed6165a5f84473830cab6f4e9bece3e74
Ken Barlow <kenbrlow@gmail.com>
2020-09-01 05:11:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,12 +8,7 @@ end
 
 def words_five_letters_long(text)
   array_text = text.split(" ")
-  puts "========================"
-  final_array = array_text.grep(/^\w{5}$/)
-  puts final_array
-  final_array
-  #puts text.scan(/\w{5}/)
-#  text.scan(/\w{5}/)
+  array_text.grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
