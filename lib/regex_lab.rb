ae1365e7fb9148541e8714d39e3c9a9f16efb0e7
Ken Barlow <kenbrlow@gmail.com>
2020-09-01 03:56:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.match? /Un[a-z]ing/i
+  text.match? /(un)[a-z](ing)/i
 end
 
 def words_five_letters_long(text)
