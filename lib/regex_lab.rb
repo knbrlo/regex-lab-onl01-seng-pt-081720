cf73ada55b8d77e1bdae15d137ffc7dc6c589d69
Ken Barlow <kenbrlow@gmail.com>
2020-09-01 05:27:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,37 +12,13 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-  # return true if first letter is capitalized and ending with punctuation
   if text.match? /^[A-Z].*\W$/
     return true
   else
     return false
   end
-
-  # return false if text starting with uncapitalized letter but ending with punctuation
-
-  # return false if text starting with capital but ending wihout punctuation
-
-  # return false if text starting without capital and ending without punctuation
-  # text.match? /\A[A-Z]$./
 end
 
 def valid_phone_number?(phone)
-  puts phone
-
-  edited_phone_number = ""
-
-  # does the phone number include hyphens?
-  if phone.to_s.include?('-')
-
-    # remove hypens
-    edited_phone_number = phone.to_s.gsub("-", "")
-
-    puts edited_phone_number
-
-
-  end
-
 
 end
