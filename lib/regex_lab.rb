6c03ecf8bd310190d9e8d11d7f1a41d88e606fae
Ken Barlow <kenbrlow@gmail.com>
2020-09-01 03:44:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if /\A[aeiou]/.match(word)
-    return true
-  else
-    return false
-  end
+  puts /\A[aeiou]/.match(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
