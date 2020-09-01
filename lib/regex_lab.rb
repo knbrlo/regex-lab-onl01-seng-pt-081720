cb9ffdf29a32cc468b275f1d71f00d4ec18e914a
Ken Barlow <kenbrlow@gmail.com>
2020-09-01 04:48:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,9 +6,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  # puts text.scan(/\w+ing/)
-  # text.scan(/\w+ing/)
-  puts text.scan(/un(.*?)ing/)
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
